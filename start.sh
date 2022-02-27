d0bef27986a0a79719cb7ebfc7afb248d75ba918
Mike <mike@Mikes-MacBook-Pro.local>
2022-02-27 14:38:22 +0200

    change

--- start.sh
+++ start.sh
@@ -1,1 +1,11 @@
-echo "Hello World!"
+#!/bin/bash -x
+DESTS=( "hello" "world" )
+TIME="180s"
+while TRUE
+do
+   for DEST in ${DESTS[@]}
+   do
+      echo "$TIME: $DEST"
+      #  docker run -ti --rm alpine/bombardier -c 1000 -d $TIME -l $DEST
+   done
+done
